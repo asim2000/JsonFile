e5358ba3790eecd08529f52f47edc5246a782f25
Asim <asimbabayev0002@gmail.com>
2021-04-09 20:19:54 +0400

    Add project files.

---
+++ Tapsiriq2/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tapsiriq2.Models;
+
+namespace Tapsiriq2.Controllers
+{
+    public class HomeController:Controller
+    {
+        [HttpGet]
+        public IActionResult CreatUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreatUser(User model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var file = System.IO.File.ReadAllText("User.json");
+            List<User> users;
+            if (!string.IsNullOrEmpty(file))
+            {
+                users = JsonConvert.DeserializeObject<List<User>>(file);
+                users.Add(model);
+            }
+            else
+            {
+                users = new List<User>();
+                users.Add(model);
+            }
+
+            WriteToJsonFile(users);
+            return Redirect("~/home/userList");
+        }
+        [HttpGet]
+        public IActionResult UserList()
+        {
+            var json=System.IO.File.ReadAllText(@"User.json");
+            
+            var users = JsonConvert.DeserializeObject<List<User>>(json);
+            if (users==null)
+            {
+                return Redirect("~/home/creatUser");
+            }
+            return View(users);
+        }
+
+        public void WriteToJsonFile(List<User> users)
+        {
+            var json = JsonConvert.SerializeObject(users);
+            System.IO.File.WriteAllText(@"User.json", json);
+        }
+    }
+    
+}
---
+++ Tapsiriq2/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tapsiriq2.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+    }
+}
